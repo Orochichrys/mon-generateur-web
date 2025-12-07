cce2749f398d0dbaeb5a3bac648b6ab537bf0f19
gitmobkab <richmondaurel77@gmail.com>
2025-12-07 21:03:50 +0000

    feat: implement BuildProjectDir function to create project directory structure

--- utility.js
+++ utility.js
@@ -1,4 +1,29 @@
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+
 export function IsProjectNameValid(ProjectName){
     const expression = /^([a-z\-\_\d])+$/;
     return expression.test(ProjectName);
+}
+
+
+export function BuildProjectDir(projectName){
+    const currentDir = process.cwd();
+    const projectDir = path.join(currentDir, projectName);
+
+  // ---------------------------------------------------------
+  // 1. CRÉATION DE LA STRUCTURE DE DOSSIERS
+  // ---------------------------------------------------------
+  if (fs.existsSync(projectDir)) {
+    console.log(chalk.red('❌ Ce dossier existe déjà.'));
+    process.exit(1);
+  }
+
+  fs.mkdirSync(projectDir);
+  fs.mkdirSync(path.join(projectDir, 'css')); // Dossier CSS
+  fs.mkdirSync(path.join(projectDir, 'js'));  // Dossier JS
+  fs.mkdirSync(path.join(projectDir, 'img')); // Dossier IMG
+  
+  console.log(chalk.green(`📁 Structure créée : ${projectName}/ (css, js, img)`));
 }
